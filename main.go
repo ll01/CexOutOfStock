fd6f1eca8eb1d124bb134a0a6f0a8c2bae851a3b
David McPherson <enti2@hotmail.co.uk>
2018-01-23 03:15:38 +0000

    added logging

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
-	import (
-	    "fmt"
-	    "net/http"
-	    "os" 
-	)
-	func handler(w http.ResponseWriter, r *http.Request) {
-	    fmt.Fprintf(w, "You just browsed page (if blank you're at the root): %s", r.URL.Path[1:])
-	}
-	func main() {
-	    http.HandleFunc("/", handler)
-	    http.ListenAndServe(":"+os.Getenv("HTTP\\_PLATFORM\\_PORT"), nil)
-	}
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
+
+func main() {
+	fmt.Println("running")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8000", nil)
+}
