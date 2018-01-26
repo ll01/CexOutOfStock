4c3d8f3863d200964bea156833eb5c522f3abef9
ll01 <enti2@hotmail.co.uk>
2018-01-26 05:06:50 +0000

    i should kill my self

--- main.go
+++ main.go
@@ -2,77 +2,31 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"strings"
-
-	"github.com/line/line-bot-sdk-go/linebot"
-	"gopkg.in/xmlpath.v2"
+	"os"
 )
 
-var isInStock = false
-var channelAccessToken string
-
-var APISecret string
-
 func main() {
-	http.HandleFunc("/", MainPage)
+	http.HandleFunc("/", handler)
 	fmt.Println("listening...")
-	err := http.ListenAndServe(":4747", nil)
+	err := http.ListenAndServe(GetPort(), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-//MainPage fuction for http response
-func MainPage(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello. This is our first Go web app on Heroku!")
 }
 
-//LineWebHook fuction for http response
-func LineWebHook(w http.ResponseWriter, r *http.Request) {
-	bot, err := linebot.New(channelAccessToken, APISecret)
-	panicError(err)
-	events, err := bot.ParseRequest(r)
-	panicError(err)
-
-	for _, event := range events {
-		if event.Type == linebot.EventTypeMessage {
-			switch event.Message.(type) {
-			case *linebot.TextMessage:
-				var message = event.Message.(*linebot.TextMessage)
-				fmt.Println(message.Text)
-			}
-
-		}
+// Get the Port from the environment so we can run on Heroku
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
 	}
-
-}
-
-func GetStockInfo(responseBody *io.ReadCloser) {
-	root, err := xmlpath.ParseHTML(*responseBody)
-	panicError(err)
-	xpath := xmlpath.MustCompile("//div[@class = \"buyNowButton\"]")
-	if stockString, ok := xpath.String(root); ok {
-		stockString = strings.TrimSpace(stockString)
-		stockString = strings.ToLower(stockString)
-		fmt.Println(stockString)
-		switch stockString {
-		case "out of stock":
-			fmt.Println("sorry :(")
-		case "i want to buy this item":
-			fmt.Println("yay in stock")
-			// email user
-		default:
-			fmt.Println("invalid url inputed ")
-
-		}
-	}
-}
-
-func panicError(err error) {
-	if err != nil {
-		panic(err)
-	}
+	return ":" + port
 }
