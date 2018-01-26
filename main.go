1da38f12e0ff9c1716b4516eec28d57f65691d18
ll01 <enti2@hotmail.co.uk>
2018-01-26 04:51:18 +0000

    test serve

--- main.go
+++ main.go
@@ -1,90 +1,32 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io"
+	"log"
 	"net/http"
-	"strings"
-
-	"github.com/line/line-bot-sdk-go/linebot"
-	"gopkg.in/xmlpath.v2"
+	"os"
 )
 
-var isInStock = false
-var APIKey *string
-
-var APISecret *string
-
-/*Need to figure out is it going to check forever and the rate at which it checks */
 func main() {
-	// get api key and secret from io
-	APIKey = flag.String("key", "", "defines the api key to acsess line bot")
-
-	if *APIKey == "" {
-		//log.Fatal("API key not given ")
-	}
-	APISecret = flag.String("secret", "", "defines the api secret to access line bot")
-
-	if *APISecret == "" {
-		//log.Fatal("API secret not given")
-	}
-
-	flag.Parse()
-	http.HandleFunc("/", MainPage)
-	http.HandleFunc("/line", LineWebHook)
-	http.ListenAndServe(":8000", nil)
-
-}
-
-//MainPage fuction for http response
-func MainPage(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello"))
-}
-
-//LineWebHook fuction for http response
-func LineWebHook(w http.ResponseWriter, r *http.Request) {
-	bot, err := linebot.New(*APIKey, "hi")
-	panicError(err)
-	events, err := bot.ParseRequest(r)
-	panicError(err)
-
-	for _, event := range events {
-		if event.Type == linebot.EventTypeMessage {
-			switch event.Message.(type) {
-			case *linebot.TextMessage:
-				var message = event.Message.(*linebot.TextMessage)
-				fmt.Println(message.Text)
-			}
-
-		}
-	}
-
-}
-
-func GetStockInfo(responseBody *io.ReadCloser) {
-	root, err := xmlpath.ParseHTML(*responseBody)
-	panicError(err)
-	xpath := xmlpath.MustCompile("//div[@class = \"buyNowButton\"]")
-	if stockString, ok := xpath.String(root); ok {
-		stockString = strings.TrimSpace(stockString)
-		stockString = strings.ToLower(stockString)
-		fmt.Println(stockString)
-		switch stockString {
-		case "out of stock":
-			fmt.Println("sorry :(")
-		case "i want to buy this item":
-			fmt.Println("yay in stock")
-			// email user
-		default:
-			fmt.Println("invalid url inputed ")
-
-		}
-	}
-}
-
-func panicError(err error) {
+	http.HandleFunc("/", handler)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(GetPort(), nil)
 	if err != nil {
-		panic(err)
+		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello. This is our first Go web app on Heroku!")
+}
+
+// Get the Port from the environment so we can run on Heroku
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
